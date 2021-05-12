c5cb090c057d9cd3dfb70180c165bfd2544c441c
M_Dev <marioxd99@gmail.com>
2021-05-12 19:14:31 +0200

    quitado bootstrap de login y register

--- src/main/java/edu/uclm/esi/carreful/http/PaymentsController.java
+++ src/main/java/edu/uclm/esi/carreful/http/PaymentsController.java
@@ -10,13 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -134,8 +131,7 @@ public class PaymentsController extends CookiesController {
 				oproduct.setTipo(new DomicilioExpress());
 			}else if(modoEnvio.equals("casa")) {
 				oproduct.setTipo(new DomicilioNormal());
-			}
-			else {
+			}else {
 				oproduct.setTipo(new RecogidaCarreful());
 			}
 			if ( email.length()==0 )
